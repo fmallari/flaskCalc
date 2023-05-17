c46cd65ea06d9eca6091f66dca99d44c058e75b9
fmallari <fc.mallari@gmail.com>
2023-05-16 23:57:07 -0700

    formulas

--- calc/app.py
+++ calc/app.py
@@ -4,3 +4,47 @@ from flask import Flask, request
 from operations import add, sub, mult, div
 
 app = Flask(__name__)
+
+@app.route("/add")
+def add_num():
+
+    """ Adding a and b parameters """
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a,b)
+
+    return str(result)
+
+@app.route("/sub")
+def sub_num():
+
+    """ Subtract a and b parameters """
+
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a,b)
+
+    return str(result)
+
+@app.route("/mult")
+def mult_num():
+
+    """ Multiply a and b parameters"""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a,b)
+
+    return str(result)
+    
+@app.route("/div")
+def div_num():
+    """ Divide a and b parameters"""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a,b)
+
+    return str(result)
